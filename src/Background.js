6d7902b8d08d15e7094be568c61eb29e8304a478
Fernanda Estrada <est14198@uvg.edu.gt>
2019-09-30 14:56:22 -0600

    corrected src/Background.js

--- src/Background.js
+++ src/Background.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-export default class Background extends Component {
-  render() {
-    return (
-      <div className="background">{this.props.children}</div>
-    )
-  }
+function Background(props) {
+  const children = props;
+  return (
+    <div className="background">{children}</div>
+  );
 }
+
+export default Background;
