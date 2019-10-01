3f5cb0741e05d411c43092542279eed3cf1aaa22
Fernanda Estrada <est14198@uvg.edu.gt>
2019-09-30 19:44:06 -0600

    corrected src/Slide.js

--- src/Slide.js
+++ src/Slide.js
@@ -1,45 +1,37 @@
-import React, { Component } from 'react';
+/* eslint-disable react/prop-types */
+import React from 'react';
 import './Slide.css';
 
-class Slide extends Component {
-  // Hola soy Rodrigo Zea, discipulo de MS 
-  // Jose Cifuentes :) discipulo de MS
-  // holis xd
-  render () {
-    const image = {
-      backgroundImage: `url(${this.props.imgPath})`,
-      width: '1000px',
-      height: '385px',
-      zoom: '0.8',
-      margin: 'auto auto auto auto'
-    }
-    
-    const bgColor = {
-      background: `#${this.props.bgColor}`
-    }
-    
-    const textColor = {
-        color: `#${this.props.txtColor}`
-      }
-    
-    const textShadow = {
-        color: `#${this.props.txtColor}`,
-        textShadow: '0 1px 4px rgba(0,0,0,0.25)'
-      } 
-    
-    return (
-      <div className="slideContainer" style={bgColor}>
-        <h1 style={this.props.shadow ? textShadow : textColor}>{this.props.title}</h1>
-        <h2 style={textColor}>{this.props.description}</h2>
-        <button>Add DuckDuckGo to Chrome</button>
-        <div className="slideImage" style={image}></div>
-      </div>
-    );
-  }
+function Slide(props) {
+  const image = {
+    backgroundImage: `url(${props.imgPath})`,
+    width: '1000px',
+    height: '385px',
+    zoom: '0.8',
+    margin: 'auto auto auto auto',
+  };
+
+  const bgColor = {
+    background: `#${props.bgColor}`,
+  };
+
+  const textColor = {
+    color: `#${props.txtColor}`,
+  };
+
+  const textShadow = {
+    color: `#${props.txtColor}`,
+    textShadow: '0 1px 4px rgba(0,0,0,0.25)',
+  };
+
+  return (
+    <div className="slideContainer" style={bgColor}>
+      <h1 style={props.shadow ? textShadow : textColor}>{props.title}</h1>
+      <h2 style={textColor}>{props.description}</h2>
+      <button type="button">Add DuckDuckGo to Chrome</button>
+      <div className="slideImage" style={image} />
+    </div>
+  );
 }
 
 export default Slide;
-
-
-
-
